e91558a06c4401d5bf1e13ba7cedcbf6352a82d8
tish0 <tish.peneff@gmail.com>
2016-09-29 09:01:56 -0400

    Added 3 activites for Lab2 and BG
    logs for debug in the life cycle functions

--- app/src/main/java/com/peneff/Lab1/ListItemsActivity.java
+++ app/src/main/java/com/peneff/Lab1/ListItemsActivity.java
@@ -1,13 +1,116 @@
 package com.peneff.Lab1;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Switch;
 public class ListItemsActivity extends AppCompatActivity {
+
+    private final String ACTIVITY_NAME = ListItemsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+
+        ImageButton cameraAct = (ImageButton)findViewById(R.id.imageButton);
+
+        cameraAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int REQUEST_IMAGE_CAPTURE = 1;
+
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // code used from lab to start camera.
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+                }
+
+            }
+        });
+
+        // 8.	Add a setOnCheckedChanged() callback handler to the Switch object
+
+        Switch mySwitch = (Switch)findViewById(R.id.switch1);
+        //set the switch to on
+        mySwitch.setChecked(true);
+
+        // attaching the listerning to check if th state changes
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Context context = getApplicationContext();
+
+                if(isChecked){ // if the switch is checked
+                    CharSequence text ="Switch is On"; // the text message that will show in the toast
+                    int duration = Toast.LENGTH_SHORT; // setting the duration the toast is on screen for
+                    Toast toast = Toast.makeText(context,text,duration); // making Toast object toast
+                    toast.show(); // displaying toast object on bottom of screen
+
+
+                }else{ // if the switch is not checked
+                    CharSequence text ="Switch is Off";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,text,duration);
+                    toast.show(); // displaying toast object on bottom of screen
+
+                }
+
+            }
+        });
+
+
+
+    Log.d(ACTIVITY_NAME, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ACTIVITY_NAME, "onStart");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ACTIVITY_NAME, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ACTIVITY_NAME, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ACTIVITY_NAME, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(ACTIVITY_NAME, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(ACTIVITY_NAME, "onRestart");
     }
 }
--- app/src/main/java/com/peneff/Lab1/LoginActivity.java
+++ app/src/main/java/com/peneff/Lab1/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.peneff.Lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,10 +10,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.PasswordAuthentication;
 
 public class LoginActivity extends AppCompatActivity {
 
-    protected static final String ACTIVITY_NAME = "LoginActivity";
+
+    EditText userField;
+    EditText passField;
+
+    private final String ACTIVITY_NAME = LoginActivity.class.getSimpleName();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,42 +32,76 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.i(ACTIVITY_NAME, "In onCreate()");
+        Log.d(ACTIVITY_NAME, "onCreate");
+
+        userField = (EditText) findViewById(R.id.userField);
+        passField = (EditText) findViewById(R.id.passField);
+
+        Button Login = (Button) findViewById(R.id.button1);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start list items screen
+                Intent log1 = new Intent(getApplicationContext(),ListItemsActivity.class );
+               startActivity(log1);
+                //start lab1 screen
+             //   Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+             //   startActivity(intent);
+
+            }
+        });
+
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(ACTIVITY_NAME, "In onStart()");
+        SharedPreferences shpr = getSharedPreferences("User info", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shpr.edit();
+        editor.putString("DefaultEmail", userField.getText().toString());
+        editor.putString("Pass", passField.getText().toString());
+        editor.apply();
+
+       // SharedPreferences shpr = getSharedPreferences("User info", Context.MODE_PRIVATE);
+
+        String name = shpr.getString("DefaultEmail","email@domain.com");
+        String pass = shpr.getString("Pass","");
+
+        userField.setText(name);
+
+
+        Log.i(ACTIVITY_NAME, "onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(ACTIVITY_NAME, "In onPause()");
+        Log.i(ACTIVITY_NAME, "onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(ACTIVITY_NAME, "In onResume()");
+        Log.i(ACTIVITY_NAME, "onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(ACTIVITY_NAME, "In onStop()");
+        Log.i(ACTIVITY_NAME, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(ACTIVITY_NAME, "In onDestroy()");
+        Log.i(ACTIVITY_NAME, "onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(ACTIVITY_NAME, "In onRestart()");
+        Log.i(ACTIVITY_NAME, "onRestart");
     }
 }
